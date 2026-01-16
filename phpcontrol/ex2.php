c1a75682aad2e455ce17311c7466f41d1c50fada
br0kxd <140101255+Br0kun@users.noreply.github.com>
2026-01-16 22:09:58 +0100

    exercice2.php

---
+++ phpcontrol/ex2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>ex2</title>
+</head>
+<body>
+    <?php
+    $note = 12;
+
+
+if ($note >= 10) {
+  echo "Admis<br>";
+} else {
+  echo "Ajourné<br>";
+}
+
+for ($i = 1; $i <= 10; $i++) {
+  echo $i . "<br> ";
+} 
+    ?>
+</body>
+</html>
