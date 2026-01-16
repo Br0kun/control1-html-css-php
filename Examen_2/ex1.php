b223ff4ae3ca0b65cc7c49e2ed2d10ef3ae9e051
br0kxd <140101255+Br0kun@users.noreply.github.com>
2026-01-16 21:58:00 +0100

    ex1

--- Examen_2/ex1.php
+++ Examen_2/ex1.php
@@ -1,1 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$nom = "Ali";
+$age = 22;
+$langages = ["PHP", "JavaScript", "Python"];
 
+¡
+echo "Bonjour $nom, vous avez $age ans.<br>";
+
+¡
+foreach ($langages as $l) {
+  echo $l . "<br>";
+}
+
+
+for ($i = 0; $i < count($langages); $i++) {
+  echo $langages[$i] . "<br>";
+}
+?>
+
+    
+</body>
+</html>
